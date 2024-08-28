5c4f8f1e87df6d8c303e03dda3ad8a13c9c645f3
Eduardo <eduardokunmixyeah@gmail.com>
2024-08-28 17:33:10 -0400

    fix arrows icons

---
+++ src/components/LandingComponets/Testimonials/testimonials.data.ts
@@ -0,0 +1,47 @@
+import imgPerfil from "./../../../assets/perfilpersona.svg";
+
+export const testimonialsData = [
+    {
+        image: imgPerfil,
+        name: "John Doe",
+        position: "CEO of Example",
+        comment:
+            "Urna malesuada blandit dui congue viverra amet rhoncus nulla. At ac quis feugiat donec venenatis nullam nam est porta. Libero pulvinar nibh.",
+        date: "2024-07-23",
+        time: "10:30 AM",
+    },
+    {
+        image: imgPerfil,
+        name: "Jane Smith",
+        position: "Marketing Director",
+        comment:
+            "Urna malesuada blandit dui congue viverra amet rhoncus nulla. At ac quis feugiat donec venenatis nullam nam est porta. Libero pulvinar nibh.",
+        date: "2024-07-22",
+        time: "1:15 PM",
+    },
+    {
+        image: imgPerfil,
+        name: "Michael Brown",
+        position: "Product Manager",
+        comment:
+            "Lorem ipsum dolor sit amet consectetur. Est vitae orci vitae ipsum. Aenean id a donec tempus. Non sed amet facilisis facilisis eget praesent. Arcu arcu maecenas vulputate odio aliquam aliquam enim sit mauris.",
+        date: "2024-07-21",
+        time: "4:00 PM",
+    },
+    {
+        image: imgPerfil,
+        name: "Emily White",
+        position: "Sales Manager",
+        comment: "The best company I have worked with.",
+        date: "2024-07-20",
+        time: "9:45 AM",
+    },
+    {
+        image: imgPerfil,
+        name: "David Wilson",
+        position: "HR Manager",
+        comment: "They exceeded my expectations in every way.",
+        date: "2024-07-19",
+        time: "2:30 PM",
+    },
+];
