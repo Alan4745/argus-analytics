1b1df4735db70515761508f26d87f04099105a5a
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2024-09-16 13:10:25 -0600

    cambios en la imagen main

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import Error404 from "./components/404/Error404";
 import Landing from "./pages/Landing/Landing";
-import './components/config/i18n';
+import "./components/config/i18n";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{ overflow: "hidden" }}>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="*" element={<Error404 />} />
